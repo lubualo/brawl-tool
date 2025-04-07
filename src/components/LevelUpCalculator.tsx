87c0031464a87559c47889e92e69c0aa9a474a65
lubualo@gmail.com <lubualo@gmail.com>
2025-04-07 20:53:38 -0300

    Added gears and hypercharge to calculator

--- src/components/LevelUpCalculator.tsx
+++ src/components/LevelUpCalculator.tsx
@@ -5,15 +5,24 @@ import LevelSelector from "@/components/LevelSelector";
 import { Separator } from "@/components/ui/separator";
 import { LevelIUpCalculatorService } from "@/utils/LevelIUpCalculatorService";
 import Image from "next/image";
+import { Switch } from "@/components/ui/switch";
 
 export default function LevelUpCalculator() {
 	const starPowerCost = 2000;
 	const gadgetCost = 1000;
+	const gearCost = 1000;
+	const epicGearCost = 1500;
+	const mythicGearCost = 2000;
+	const hyperchargeCost = 5000;
 
 	const [initialLevel, setInitialLevel] = useState(1);
 	const [targetLevel, setTargetLevel] = useState(11);
 	const [starPowers, setStarPowers] = useState(0);
 	const [gadgets, setGadgets] = useState(0);
+	const [gears, setGears] = useState(0);
+	const [epicGears, setEpicGears] = useState(0);
+	const [mythicGears, setMythicGears] = useState(0);
+	const [hypercharge, setHypercharge] = useState(0);
 
 	return (
 		<>
@@ -48,10 +57,50 @@ export default function LevelUpCalculator() {
 			<div className="flex w-full pb-4">
 				<div className="basis-1/2">
 					<div className="flex items-center justify-center text-sm font-bold pb-1">
+						Gadgets
+						<Image
+							src="/gadget-icon.png"
+							alt="Gadget icon"
+							width={16}
+							height={16}
+							className="ml-2"
+						/>
+					</div>
+					<LevelSelector
+						level={gadgets}
+						onLevelChange={(newLevel: number) =>
+							setGadgets(newLevel)
+						}
+						min={0}
+						max={2}
+					/>
+				</div>
+				<div className="basis-1/2">
+					<div className="flex items-center justify-center text-sm font-bold pb-1">
+						Gears
+						<Image
+							src="/gear-super-rare-icon.png"
+							alt="Gear icon"
+							width={16}
+							height={16}
+							className="ml-2"
+						/>
+					</div>
+					<LevelSelector
+						level={gears}
+						onLevelChange={(newLevel: number) => setGears(newLevel)}
+						min={0}
+						max={2}
+					/>
+				</div>
+			</div>
+			<div className="flex w-full pb-4">
+				<div className="basis-1/2">
+					<div className="flex items-center justify-center text-sm font-bold pb-1">
 						Star powers
 						<Image
 							src="/star-power-icon.png"
-							alt="Coins icon"
+							alt="Star powers icon"
 							width={16}
 							height={16}
 							className="ml-2"
@@ -68,22 +117,59 @@ export default function LevelUpCalculator() {
 				</div>
 				<div className="basis-1/2">
 					<div className="flex items-center justify-center text-sm font-bold pb-1">
-						Gadgets
+						Epic gears
 						<Image
-							src="/gadget-icon.png"
-							alt="Coins icon"
+							src="/gear-epic-icon.png"
+							alt="Epic gear icon"
 							width={16}
 							height={16}
 							className="ml-2"
 						/>
 					</div>
 					<LevelSelector
-						level={gadgets}
+						level={epicGears}
 						onLevelChange={(newLevel: number) =>
-							setGadgets(newLevel)
+							setEpicGears(newLevel)
 						}
 						min={0}
-						max={2}
+						max={1}
+					/>
+				</div>
+			</div>
+			<div className="flex w-full pb-4 justify-center">
+				<div className="basis-1/2">
+					<div className="flex items-center justify-center text-sm font-bold pb-1">
+						Hypercharge
+						<Image
+							src="/hyper-charge-icon.png"
+							alt="Hypercharges icon"
+							width={16}
+							height={16}
+							className="ml-2"
+						/>
+					</div>
+					<div className="flex justify-center">
+						<Switch id="hypercharge" onCheckedChange={(checked) => setHypercharge(checked ? 1 : 0)}/>
+					</div>
+				</div>
+				<div className="basis-1/2">
+					<div className="flex items-center justify-center text-sm font-bold pb-1">
+						Mythic gears
+						<Image
+							src="/gear-mythic-icon.png"
+							alt="Mythic gear icon"
+							width={16}
+							height={16}
+							className="ml-2"
+						/>
+					</div>
+					<LevelSelector
+						level={mythicGears}
+						onLevelChange={(newLevel: number) =>
+							setMythicGears(newLevel)
+						}
+						min={0}
+						max={1}
 					/>
 				</div>
 			</div>
@@ -92,12 +178,13 @@ export default function LevelUpCalculator() {
 				<div className="basis-1/2 text-center">
 					<div className="text-sm font-bold pb-1">Coins</div>
 					<div className="flex items-center justify-center text-l">
-						{LevelIUpCalculatorService.calculateCoinCost(
-							initialLevel,
-							targetLevel
-						) +
+						{LevelIUpCalculatorService.calculateCoinCost( initialLevel, targetLevel ) +
 							starPowers * starPowerCost +
-							gadgets * gadgetCost}
+							gadgets * gadgetCost +
+							gears * gearCost +
+							epicGears * epicGearCost +
+							mythicGears * mythicGearCost +
+							hypercharge * hyperchargeCost}
 						<Image
 							src="/coin-icon.svg"
 							alt="Coins icon"
