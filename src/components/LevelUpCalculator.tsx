43758f6d4665ab2bac829319179ed8c95d2f1bb5
lubualo@gmail.com <lubualo@gmail.com>
2025-04-07 18:13:47 -0300

    Add icons to labels

--- src/components/LevelUpCalculator.tsx
+++ src/components/LevelUpCalculator.tsx
@@ -24,7 +24,9 @@ export default function LevelUpCalculator() {
 					</div>
 					<LevelSelector
 						level={initialLevel}
-						onLevelChange={(newLevel: number) => setInitialLevel(newLevel) }
+						onLevelChange={(newLevel: number) =>
+							setInitialLevel(newLevel)
+						}
 						min={1}
 						max={targetLevel - 1}
 					/>
@@ -35,7 +37,9 @@ export default function LevelUpCalculator() {
 					</div>
 					<LevelSelector
 						level={targetLevel}
-						onLevelChange={(newLevel: number) => setTargetLevel(newLevel) }
+						onLevelChange={(newLevel: number) =>
+							setTargetLevel(newLevel)
+						}
 						min={initialLevel + 1}
 						max={11}
 					/>
@@ -43,23 +47,41 @@ export default function LevelUpCalculator() {
 			</div>
 			<div className="flex w-full pb-4">
 				<div className="basis-1/2">
-					<div className="text-sm text-center font-bold pb-1">
+					<div className="flex items-center justify-center text-sm font-bold pb-1">
 						Star powers
+						<Image
+							src="/star-power-icon.png"
+							alt="Coins icon"
+							width={16}
+							height={16}
+							className="ml-2"
+						/>
 					</div>
 					<LevelSelector
 						level={starPowers}
-						onLevelChange={(newLevel: number) => setStarPowers(newLevel) }
+						onLevelChange={(newLevel: number) =>
+							setStarPowers(newLevel)
+						}
 						min={0}
 						max={2}
 					/>
 				</div>
 				<div className="basis-1/2">
-					<div className="text-sm text-center font-bold pb-1">
+					<div className="flex items-center justify-center text-sm font-bold pb-1">
 						Gadgets
+						<Image
+							src="/gadget-icon.png"
+							alt="Coins icon"
+							width={16}
+							height={16}
+							className="ml-2"
+						/>
 					</div>
 					<LevelSelector
 						level={gadgets}
-						onLevelChange={(newLevel: number) => setGadgets(newLevel) }
+						onLevelChange={(newLevel: number) =>
+							setGadgets(newLevel)
+						}
 						min={0}
 						max={2}
 					/>
@@ -69,12 +91,13 @@ export default function LevelUpCalculator() {
 			<div className="flex w-full pb-4">
 				<div className="basis-1/2 text-center">
 					<div className="text-sm font-bold pb-1">Coins</div>
-                    <div className="flex items-center justify-center text-l">
-						{
-							LevelIUpCalculatorService.calculateCoinCost( initialLevel, targetLevel )
-							+ starPowers * starPowerCost
-							+ gadgets * gadgetCost
-							}
+					<div className="flex items-center justify-center text-l">
+						{LevelIUpCalculatorService.calculateCoinCost(
+							initialLevel,
+							targetLevel
+						) +
+							starPowers * starPowerCost +
+							gadgets * gadgetCost}
 						<Image
 							src="/coin-icon.svg"
 							alt="Coins icon"
@@ -86,8 +109,11 @@ export default function LevelUpCalculator() {
 				</div>
 				<div className="basis-1/2 text-center">
 					<div className="text-sm font-bold pb-1">Power points</div>
-                    <div className="flex items-center justify-center text-l">
-						{LevelIUpCalculatorService.calculatePowerPoints( initialLevel, targetLevel )}
+					<div className="flex items-center justify-center text-l">
+						{LevelIUpCalculatorService.calculatePowerPoints(
+							initialLevel,
+							targetLevel
+						)}
 						<Image
 							src="/power-point-icon.svg"
 							alt="Power points icon"
